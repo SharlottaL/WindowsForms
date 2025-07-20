e4b72cd010c22f87d714c15256a5dd712bedc7e5
SharlottaL <botanichkagigantika@gmail.com>
2025-07-20 12:22:14 +0300

    Time && Date && Day  DONE

--- WindowsForms/MainForm.Designer.cs
+++ WindowsForms/MainForm.Designer.cs
@@ -30,34 +30,84 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.labelCurrentTime = new System.Windows.Forms.Label();
-            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.labelCurrentTime1 = new System.Windows.Forms.Label();
+            this.CurrentData = new System.Windows.Forms.Label();
+            this.labelCurrentDay = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // labelCurrentTime
+            // labelCurrentTime1
             // 
-            this.labelCurrentTime.AutoSize = true;
-            this.labelCurrentTime.Font = new System.Drawing.Font("Ravie", 32.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCurrentTime.Location = new System.Drawing.Point(13, 13);
-            this.labelCurrentTime.Name = "labelCurrentTime";
-            this.labelCurrentTime.Size = new System.Drawing.Size(406, 57);
-            this.labelCurrentTime.TabIndex = 0;
-            this.labelCurrentTime.Text = "currentTime";
+            this.labelCurrentTime1.AutoSize = true;
+            this.labelCurrentTime1.Font = new System.Drawing.Font("Script MT Bold", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCurrentTime1.Location = new System.Drawing.Point(19, 9);
+            this.labelCurrentTime1.Name = "labelCurrentTime1";
+            this.labelCurrentTime1.Size = new System.Drawing.Size(264, 58);
+            this.labelCurrentTime1.TabIndex = 0;
+            this.labelCurrentTime1.Text = "CurrentTime";
             // 
-            // timer
+            // CurrentData
             // 
-            this.timer.Enabled = true;
-            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            this.CurrentData.AutoSize = true;
+            this.CurrentData.Font = new System.Drawing.Font("Script MT Bold", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentData.Location = new System.Drawing.Point(19, 86);
+            this.CurrentData.Name = "CurrentData";
+            this.CurrentData.Size = new System.Drawing.Size(266, 58);
+            this.CurrentData.TabIndex = 1;
+            this.CurrentData.Text = "CurrentData";
+            // 
+            // labelCurrentDay
+            // 
+            this.labelCurrentDay.AutoSize = true;
+            this.labelCurrentDay.Font = new System.Drawing.Font("Script MT Bold", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCurrentDay.Location = new System.Drawing.Point(19, 163);
+            this.labelCurrentDay.Name = "labelCurrentDay";
+            this.labelCurrentDay.Size = new System.Drawing.Size(253, 58);
+            this.labelCurrentDay.TabIndex = 2;
+            this.labelCurrentDay.Text = "CurrentDay";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.Location = new System.Drawing.Point(249, 138);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(239, 38);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Показать дату";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.BackColor = System.Drawing.SystemColors.Control;
+            this.checkBox4.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox4.Location = new System.Drawing.Point(249, 224);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(355, 38);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "Показать день недели";
+            this.checkBox4.UseVisualStyleBackColor = false;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.labelCurrentTime);
+            this.ClientSize = new System.Drawing.Size(793, 486);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.labelCurrentDay);
+            this.Controls.Add(this.CurrentData);
+            this.Controls.Add(this.labelCurrentTime1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
-            this.Text = "Clock";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +117,15 @@
 
         private System.Windows.Forms.Label labelCurrentTime;
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCurrentTime1;
+        private System.Windows.Forms.Label CurrentData;
+        private System.Windows.Forms.Label labelCurrentDay;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- WindowsForms/MainForm.cs
+++ WindowsForms/MainForm.cs
@@ -17,9 +17,21 @@ namespace WindowsForms
             InitializeComponent();
         }
 
-        private void timer_Tick(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            labelCurrentTime.Text = DateTime.Now.ToString("hh:mm:ss tt");
+            labelCurrentTime1.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            CurrentData.Text = DateTime.Now.ToString("MMMM dd, yyyy");
+            //https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.datetimepicker?view=windowsdesktop-9.0#:~:text=%22MMMM%20dd%2C%20yyyy%20%2D%20dddd%22
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            labelCurrentDay.Text = DateTime.Now.ToString("dddd");
+            //https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.datetimepicker?view=windowsdesktop-9.0#:~:text=%22MMMM%20dd%2C%20yyyy%20%2D%20dddd%22
         }
     }
 }
