60719f846b8fef30a26b210e2a6d2eea787983f9
SharlottaL <botanichkagigantika@gmail.com>
2025-07-23 12:47:05 +0300

    context menu

--- WindowsForms/MainForm.cs
+++ WindowsForms/MainForm.cs
@@ -20,6 +20,20 @@ namespace WindowsForms
         private void timer_Tick(object sender, EventArgs e)
         {
             labelCurrentTime.Text = DateTime.Now.ToString("hh:mm:ss tt");
+            labelCurrentTime1.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            CurrentData.Text = DateTime.Now.ToString("MMMM dd, yyyy");
+            //https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.datetimepicker?view=windowsdesktop-9.0#:~:text=%22MMMM%20dd%2C%20yyyy%20%2D%20dddd%22
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            labelCurrentDay.Text = DateTime.Now.ToString("dddd");
+            //https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.datetimepicker?view=windowsdesktop-9.0#:~:text=%22MMMM%20dd%2C%20yyyy%20%2D%20dddd%22
+
         }
     }
 }
